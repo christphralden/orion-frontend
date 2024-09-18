35a06d0c75cd342f1a81cc680c51b9b1060a025f
christopher-alden <christopheralden04@gmail.com>
2024-09-18 19:45:35 +0700

    feat: initial login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,38 @@
 import { Button } from "@components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@components/ui/card";
+import { Input } from "@components/ui/input";
 
 const Landing = () => {
   return (
     <div className="w-full h-screen bg-[#fafafa] flex justify-center items-center">
-      <Button>
-        <h1 className="color-black text-5xl font-bold uppercase">Orion</h1>
-      </Button>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Welcome to Orion.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Input id="name" placeholder="Username" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Input id="name" placeholder="Password" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button className="w-full">Login</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 };

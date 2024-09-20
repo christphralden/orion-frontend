05d3517f7ac9f94286b86daa4a124c17596675f8
RV <100607241+Sushrimp@users.noreply.github.com>
2024-09-20 13:27:22 +0700

    feat: ui home

--- src/modules/core/components/navbar/navbar.tsx
+++ src/modules/core/components/navbar/navbar.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "@radix-ui/react-separator";
 import {
     NavigationMenu,
     NavigationMenuContent,
@@ -8,28 +9,51 @@ import {
     NavigationMenuTrigger,
     NavigationMenuViewport,
 } from "../ui/navigation-menu"
+// import {
+//     DropdownMenu,
+//     DropdownMenuContent,
+//     DropdownMenuItem,
+//     DropdownMenuLabel,
+//     DropdownMenuSeparator,
+//     DropdownMenuTrigger,
+// } from "../ui/dropdown-menu"
 import { navigationMenuTriggerStyle } from "../ui/navigation-menu"
+import { Label } from "@radix-ui/react-dropdown-menu";
+import { Button } from "@components/ui/button";
 
 const NavBar = () => {
     return (
-        <div className="cursor-pointer">
-            <NavigationMenu className="border-b-2 mb-6">
+        <div className="cursor-pointer mb-6 flex items-center w-[1000px] justify-between">
+            <NavigationMenu>
                 <NavigationMenuList>
                     <NavigationMenuItem>
-                        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                        <NavigationMenuTrigger>Case Making</NavigationMenuTrigger>
                         <NavigationMenuContent>
                             <NavigationMenuLink>Link</NavigationMenuLink>
                         </NavigationMenuContent>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                            Item Two
+                            Correction
                         </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem> 
+                        <NavigationMenuTrigger>TY23-1</NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                            <div className="flex p-2">
+                                <Button variant="ghost" className="">
+                                    Sign Out
+                                </Button>
+                            </div>
+                        </NavigationMenuContent>
                     </NavigationMenuItem>
                 </NavigationMenuList>
             </NavigationMenu>
         </div>
     );
 };
-
 export default NavBar;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,34 @@
 import NavBar from "@components/navbar/navbar";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@components/ui/card";
 import { Separator } from "@components/ui/separator"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@components/ui/dropdown-menu"
+import { Button } from "@components/ui/button";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen justify-center items-center flex flex-col">
+    <div className="w-full h-screen items-center flex flex-col mt-3">
+      <div className="flex justify-between w-[1000px]">
+        <div>Logo</div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline">Odd Semester 24/25</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Even Semester 23/24</DropdownMenuItem>
+            <DropdownMenuItem>Odd Semester 23/24</DropdownMenuItem>
+            <DropdownMenuItem>Even Semester 22/23</DropdownMenuItem>
+            <DropdownMenuItem>Odd Semester 22/23</DropdownMenuItem>
+          </DropdownMenuContent>
+      </DropdownMenu>
+      </div>
+
       <NavBar />
 
       <Card className="w-[1000px]">
@@ -15,12 +39,36 @@ const Home = () => {
         <Separator className="mb-5"/>
         <CardContent>
           <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              asdasa
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              asdas
-            </div>
+            <Button variant="ghost" className="flex h-10 items-center w-[100%]">
+              <div className="w-[10%]">Type</div>
+              <Separator orientation="vertical" />
+              <div className="w-[40%]">Matkul</div>
+              <Separator orientation="vertical" />
+              <div className="w-[7.5%]">Room</div>
+              <Separator orientation="vertical" />
+              <div className="w-[7.5%]">Class</div>
+              <Separator orientation="vertical" />
+              <div className="w-[15%]">Start Time</div>
+              <Separator orientation="vertical" />
+              <div className="w-[15%]">End Time</div>
+              <Separator orientation="vertical" />
+              <div className="w-[10%]">Status</div>
+            </Button>
+            <Button variant="ghost" className="flex h-10 items-center w-[100%]">
+              <div className="w-[10%]">Teaching</div>
+              <Separator orientation="vertical" />
+              <div className="w-[40%]">LANG6969001 - Racism Neural Network</div>
+              <Separator orientation="vertical" />
+              <div className="w-[7.5%]">A1501</div>
+              <Separator orientation="vertical" />
+              <div className="w-[7.5%]">JE01</div>
+              <Separator orientation="vertical" />
+              <div className="w-[15%]">07:20</div>
+              <Separator orientation="vertical" />
+              <div className="w-[15%]">19:20</div>
+              <Separator orientation="vertical" />
+              <div className="w-[10%]">Ongoing</div>
+            </Button>
           </div>
         </CardContent>
         {/* <CardFooter className="flex justify-between">
